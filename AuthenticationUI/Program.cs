66fad2813f6539e9adac57e3ed79fcb2f90f5177
DeltaVio <daria.shvets.74@gmail.com>
2024-07-25 11:57:19 +0300

    Add logout Add <AuthorizeView> in Main Layout

--- AuthenticationUI/Program.cs
+++ AuthenticationUI/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddHttpClient();
+builder.Services.AddCascadingAuthenticationState();
+
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri("https://localhost:7267") // URL
