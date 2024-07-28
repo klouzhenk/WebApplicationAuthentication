be491dd1e165a8a965568ec0cfa3c01caeb901a0
DeltaVio <daria.shvets.74@gmail.com>
2024-07-28 20:00:38 +0300

    Update weather

---
+++ AuthenticationUI/Components/Pages/Weather.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using AuthenticationUI.Components.Pages.Model;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace AuthenticationUI
+{
+    [Authorize]
+    public partial class WeatherClass : ComponentBase
+    {
+        public WeatherForecast[]? forecasts;
+        [Inject] private HttpClient Http { get; set; } = default!;
+        [Inject] private AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = default!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            if (!authState.User.Identity.IsAuthenticated)
+            {
+                // Перенаправлення на сторінку входу або повідомлення про помилку
+                NavigationManager.NavigateTo("/");
+                return;
+            }
+
+            try
+            {
+                forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+            }
+            catch (HttpRequestException ex)
+            {
+                // Логування помилки
+                Console.Error.WriteLine($"Failed to fetch weather data: {ex.Message}");
+            }
+        }
+    }
+}
--- WebApplicationAuthentication/Controllers/WeatherForecastController.cs
+++ WebApplicationAuthentication/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace WebApplicationAuthentication.Controllers
 {
