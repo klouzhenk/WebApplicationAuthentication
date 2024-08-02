5bfa1903f7b38c5883956722c0449b737bab3b62
Klouz <yuliavydryk@gmail.com>
2024-08-02 13:07:14 +0300

    Rebuilding counter page as information page.

--- AuthenticationUI/Components/Pages/Model/UserModel.cs
+++ AuthenticationUI/Components/Pages/Model/UserModel.cs
@@ -4,6 +4,7 @@ public class UserModel
 {
     public string Name { get; set; }
     public string Password { get; set; }
+    public string Role { get; set; }
 
     public static UserModel GetUserInfoFromClaims(ClaimsPrincipal user)
     {
@@ -12,8 +13,12 @@ public class UserModel
         if (user.Identity.IsAuthenticated)
         {
             var claims = user.Claims;
+
             var nameClaim = user.FindFirstValue(ClaimTypes.Name);
             if (nameClaim != null) { userModel.Name = nameClaim; }
+
+            var roleClaim = user.FindFirstValue(ClaimTypes.Role);
+            if (roleClaim != null) { userModel.Role = roleClaim; }
         }
 
         return userModel;
