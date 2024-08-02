9866e28495b6b6f53ebdf342d09219bb9f8a96a3
Klouz <yuliavydryk@gmail.com>
2024-08-02 17:18:04 +0300

    Fix bug with first rendering weather page.

--- AuthenticationUI/Components/Pages/Weather.razor.cs
+++ AuthenticationUI/Components/Pages/Weather.razor.cs
@@ -12,45 +12,45 @@ namespace AuthenticationUI.Components.Pages
         [Inject] private CustomAuthStateProvider CustomAuthStateProvider { get; set; }
         [Inject] private NavigationManager NavigationManager { get; set; }
 
-        protected override async Task OnInitializedAsync()
-        {
-            var authState = await CustomAuthStateProvider.GetAuthenticationStateAsync();
-            if (!authState.User.Identity.IsAuthenticated)
-            {
-                NavigationManager.NavigateTo("/");
-                return;
-            }
-
-            try
-            {
-                var idTownClaim = authState.User.FindFirst("IdTown");
-                if (idTownClaim != null)
-                {
-                    string idTown = idTownClaim.Value;
-                    string day = DateTime.Today.ToString("dd.MM.yyyy");
-
-                    var url = $"WeatherForecast?idTown={idTown}&day={day}";
-                    forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>(url);
-                }
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.Error.WriteLine($"Failed to fetch weather data: {ex.Message}");
-            }
-        }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
             {
                 CustomAuthStateProvider.CheckAuthenticationAfterRendering();
-            }
-            var authState = await CustomAuthStateProvider.GetAuthenticationStateAsync();
 
-            if (!authState.User.Identity.IsAuthenticated)
-            {
-                NavigationManager.NavigateTo("/");
-                return;
+                var authState = await CustomAuthStateProvider.GetAuthenticationStateAsync();
+
+                if (!authState.User.Identity.IsAuthenticated)
+                {
+                    NavigationManager.NavigateTo("/");
+                    return;
+                }
+
+                if (!authState.User.Identity.IsAuthenticated)
+                {
+                    NavigationManager.NavigateTo("/");
+                    return;
+                }
+
+                try
+                {
+                    var idTownClaim = authState.User.FindFirst("IdTown");
+                    if (idTownClaim != null)
+                    {
+                        string idTown = idTownClaim.Value;
+                        string day = DateTime.Today.ToString("dd.MM.yyyy");
+
+                        var url = $"WeatherForecast?idTown={idTown}&day={day}";
+                        forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>(url);
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.Error.WriteLine($"Failed to fetch weather data: {ex.Message}");
+                }
+
+                StateHasChanged();
             }
         }
     }
