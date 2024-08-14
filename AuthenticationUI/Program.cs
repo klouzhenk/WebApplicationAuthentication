ad6743647eb8c1de5f3bc25f25885973641427ae
Yuliia Vydryk <yvydryk@mint-innovations.com>
2024-08-14 11:08:25 +0300

    Make sharing code in next way: Login.cs and AuthStateProvider in the Web App folder, pass variables from class like parameters (including methods).

--- AuthenticationUI/Program.cs
+++ AuthenticationUI/Program.cs
@@ -47,8 +47,8 @@ builder.Services.AddAuthenticationCore();
 builder.Services.AddAuthorizationCore();
 
 // Register CustomAuthStateProvider from the WebApplicationShared RCL
-builder.Services.AddScoped<CustomAuthStateProvider>();
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddScoped<WebApplicationShared.CustomAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider, WebApplicationShared.CustomAuthStateProvider>();
 
 //builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<CustomAuthStateProvider>());
 builder.Services.AddSingleton<JwtSecurityTokenHandler>();
