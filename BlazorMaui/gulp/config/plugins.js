8cb01af97b7ead1eb965bbaa42f95d082545f4b1
Yuliia Vydryk <yvydryk@mint-innovations.com>
2024-08-15 16:13:47 +0300

    Remove unnecessary files. Fix UI of login page (add margin-top when our screen size is between 1200px and ~1150px).

--- BlazorMaui/gulp/config/plugins.js
+++
@@ -1,9 +0,0 @@
-import plumber from "gulp-plumber";
-import notify from "gulp-notify";
-import newer from "gulp-newer";
-
-export const plugins = {
-  plumber: plumber,
-  notify: notify,
-  newer: newer,
-};
